ea846637d3feafe7ba35172df5db95f1fad4d78f
Yashika <yashikajotwani12@gmail.com>
2021-06-26 22:01:43 +0530

    done

--- Poll/settings.py
+++ Poll/settings.py
@@ -69,8 +69,8 @@ TEMPLATES = [
         },
     },
 ]
-# STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 WSGI_APPLICATION = 'Poll.wsgi.application'
 
 
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
